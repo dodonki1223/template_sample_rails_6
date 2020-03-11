7e19abc855fee9e54c588066725b49220efeeb78
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-03-11 09:33:08 +0900

    :wrench: Factory Botの自動生成ディレクトリ設定の変更

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'factory_bot_rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,5 +18,9 @@ module DockerSampleRails6
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Factory bot generate file path settings
+    # test/factories/*.rb -> spec/factories/*.rb
+    config.factory_bot.definition_file_paths = ['spec/factories']
   end
 end
